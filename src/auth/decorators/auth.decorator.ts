fc84e9aef9f1ac9bbc8df082d7ba78e8c58a57fd
icediver <ageev75@gmail.com>
2023-10-27 10:19:54 +0300

    feature(auth) --add registration

---
+++ src/auth/decorators/auth.decorator.ts
@@ -0,0 +1,11 @@
+import { applyDecorators, UseGuards } from '@nestjs/common';
+import { OnlyAdminGuard } from '../guards/admin.guard';
+import { JwtAuthGuard } from '../guards/jwt.guard';
+import { TypeRole } from '../types/auth.interface';
+
+export const Auth = (role: TypeRole = 'user') =>
+	applyDecorators(
+		role === 'admin'
+			? UseGuards(JwtAuthGuard, OnlyAdminGuard)
+			: UseGuards(JwtAuthGuard),
+	);
